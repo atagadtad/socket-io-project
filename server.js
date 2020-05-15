930801be9c4ff48b47f5d82fef55c80357977b26
Aaron Tagadtad <atagadtad@creativeguild.ca>
2020-05-14 21:39:46 -0400

    renamed chatServer.js to server.js

--- server.js
+++ server.js
@@ -1,5 +1,44 @@
-const http = require("./app");
+const express = require("express");
+const http = require("http");
+const app = express();
+const server = http.createServer(app);
+const socket = require("socket.io");
+const io = socket(server);
 
-http.listen(3001, () => {
-  console.log("Server running on port 3001");
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 8000;
+}
+
+const users = {};
+
+app.get("/", (req, res) => {
+  res.send("<h1>Hello world</h1>");
 });
+
+io.on("connection", (socket) => {
+  console.log("User connected on port 8000");
+  if (!users[socket.id]) {
+    users[socket.id] = socket.id;
+  }
+
+  socket.emit("yourID", socket.id);
+  io.sockets.emit("allUsers", users);
+
+  socket.on("disconnect", () => {
+    delete users[socket.id];
+  });
+
+  socket.on("callUser", (data) => {
+    io.to(data.userToCall).emit("hey", {
+      signal: data.signalData,
+      from: data.from,
+    });
+  });
+
+  socket.on("acceptCall", (data) => {
+    io.to(data.to).emit("callAccepted", data.signal);
+  });
+});
+
+server.listen(port, () => console.log("server is running on port 3001"));
