a81b314512c172e5f94b8463e0379aa6cc21bbf6
Aaron Tagadtad <atagadtad@creativeguild.ca>
2020-05-14 21:31:39 -0400

    push for heroku

--- chatServer.js
+++ chatServer.js
@@ -1,13 +1,39 @@
-const connectionArray = [];
+const express = require("express");
+const http = require("http");
+const app = express();
+const server = http.createServer(app);
+const socket = require("socket.io");
+const io = socket(server);
 
+const users = {};
 
-const sendToOneUser = (target, msgString) => {
-  const isUnique = true;
-  const i; 
+app.get("/", (req, res) => {
+  res.send("<h1>Hello world</h1>");
+});
 
-  connectionArray.forEach((wsObj, i) => {
-    if (wsObj.userName === target) {
-      wsObj.send(msgString);
-    }
-  })
-}
+io.on("connection", (socket) => {
+  console.log("User connected on port 8000");
+  if (!users[socket.id]) {
+    users[socket.id] = socket.id;
+  }
+
+  socket.emit("yourID", socket.id);
+  io.sockets.emit("allUsers", users);
+
+  socket.on("disconnect", () => {
+    delete users[socket.id];
+  });
+
+  socket.on("callUser", (data) => {
+    io.to(data.userToCall).emit("hey", {
+      signal: data.signalData,
+      from: data.from,
+    });
+  });
+
+  socket.on("acceptCall", (data) => {
+    io.to(data.to).emit("callAccepted", data.signal);
+  });
+});
+
+server.listen(3001, () => console.log("server is running on port 3001"));
